af1b925aebd5be912c27bbaea88d40591c3fd078
anshul92101 <anshul92101@gmail.com>
2022-04-01 17:25:37 +0530

    Time: 33 ms (42.62%), Space: 10.6 MB (88.61%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
     int dp[1000][1000];
-    int lcs(string &text1, string &text2, int n1, int n2){
+    int getAnswer(string &text1, string &text2, int N){
         
-        if(n1==0||n2==0) return 0;
-        
-        if(dp[n1][n2]!=0) return dp[n1][n2];
-        
-        if(text1[n1-1]==text2[n2-1]){
-            
-            return dp[n1][n2] = 1 + lcs(text1,text2,n1-1,n2-1);
-            
-        }
-        else{
-            
-            return dp[n1][n2] = max(lcs(text1,text2,n1,n2-1), lcs(text1,text2,n1-1,n2));
-            
+        int lcs[2][N+1];
+        memset(lcs,0,sizeof lcs);
+        for(int i=0;i<2;i++){
+            for(int j=0;j<N;j++){
+                if(i==0 || j==0) lcs[i][j]=0;
+            }
         }
         
+        for(int i=1;i<=text1.size();i++){
+            for(int j=1;j<=N;j++){
+                if(text1[i-1]==text2[j-1]) lcs[i%2][j] = 1 + lcs[(i+1)%2][(j-1)];
+                else lcs[i%2][j] = max(lcs[(i+1)%2][j],lcs[i%2][(j-1)]);
+            }
+        }
         
-        
+        return lcs[text1.size()%2][N];
     }
     
     
@@ -27,9 +26,9 @@ public:
         
         int n1 = text1.size();
         int n2 = text2.size();
-        memset(dp,0,sizeof dp);
        // vector<vector<int>> dp(n1+1,vector<int>(n2+1,0));
-        int ans = lcs(text1,text2,n1,n2);    
+        int biggerLength = max(n1,n2);
+        int ans = getAnswer(text1,text2, biggerLength);    
         return ans;
     }
 };
