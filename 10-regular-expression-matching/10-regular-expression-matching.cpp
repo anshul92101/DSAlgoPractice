157ce324c13c1b3f2baace9507c536acc989b724
anshul92101 <anshul92101@gmail.com>
2022-09-04 19:44:52 +0530

    Time: 16 ms (37.70%), Space: 18.4 MB (14.54%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.cpp
+++ 10-regular-expression-matching/10-regular-expression-matching.cpp
@@ -1,38 +1,38 @@
 class Solution {
 public:
     
-    bool getIfMatching(int i, int j, string& p, string& s, vector<vector<int>>& dp){
+    //cases --> s->i , p --> j
+    //match from the end
+    
+    bool getIfMatching(string s, string p, int i, int j, vector<vector<int>>& dp){
         
-        if(i<=-1 && j<=-1) return true;
-        if(i<0 && j>=0) return false;
-        if(j==-1 && i>=0){
-            if(p[i]=='*') return getIfMatching(i-2,j,p,s,dp);
+        if(j<0 && i<0) return true;
+        if(j<0 && i>=0) return false;
+        if(j>=0 && i<0){
+            if(p[j]=='*') return getIfMatching(s,p,i,j-2,dp);
             else return false;
         }
-        
-        
         if(dp[i][j]!=-1) return dp[i][j];
-        if(p[i]==s[j] || p[i]=='.') return dp[i][j] = getIfMatching(i-1,j-1,p,s,dp);
-        else if(p[i]=='*'){ 
-            
-            if(p[i-1]==s[j] || p[i-1]=='.'){
-                return dp[i][j] = getIfMatching(i-2,j,p,s,dp) || getIfMatching(i,j-1,p,s,dp) ;
-            }
-            else return dp[i][j] = getIfMatching(i-2,j,p,s,dp);
-                
-        }
-        return dp[i][j] = false;
         
+        
+        if(s[i]==p[j] || p[j]=='.') return dp[i][j] = getIfMatching(s,p,i-1,j-1,dp);
+        if(p[j]=='*'){
+            if(p[j-1]==s[i] || p[j-1]=='.') return dp[i][j] = getIfMatching(s,p,i-1,j,dp) || getIfMatching(s,p,i,j-2,dp);
+            else return dp[i][j] = getIfMatching(s,p,i,j-2,dp);
+        }
+           
+        return dp[i][j] = false;
+           
     }
     
     
     bool isMatch(string s, string p) {
         
-        int m = p.size();
-        int n = s.size();        
+        int m = s.length();
+        int n = p.length();
         vector<vector<int>> dp(m,vector<int>(n,-1));
-        bool ans = getIfMatching(m-1,n-1,p,s,dp);
-        return ans;
-        
+        bool solution = getIfMatching(s,p,m-1,n-1,dp);
+        return solution;
+    
     }
 };
