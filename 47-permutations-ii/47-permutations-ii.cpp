3d81881419e3337b38affea8866b8cbfd52e6291
anshul92101 <anshul92101@gmail.com>
2022-09-04 18:24:48 +0530

    Time: 27 ms (36.94%), Space: 11.9 MB (17.90%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    void findSolutions(vector<int> &nums, int currIndex,vector<vector<int>> & solutions ){
-            
+    void findSolutions(vector<int> nums, int currIndex,vector<vector<int>> &solutions ){
+
         if(currIndex==nums.size()){
             solutions.push_back(nums);
             return;
@@ -9,19 +9,14 @@ public:
         
         int n = nums.size();
         unordered_set<int> hashSet;
-        hashSet.clear();
-        
         for(int i=currIndex;i<n;i++){
             if(hashSet.find(nums[i])==hashSet.end()){
                 hashSet.insert(nums[i]);
+                swap(nums[currIndex],nums[i]);
+                findSolutions(nums,currIndex+1,solutions);
+                swap(nums[currIndex],nums[i]);
             }
-            else continue;
-            swap(nums[i],nums[currIndex]);
-            findSolutions(nums,currIndex+1,solutions);
-            swap(nums[i],nums[currIndex]);
         }
-        
-        
         
     }
     
